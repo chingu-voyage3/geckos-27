eb33b18486b3d646b3323f13009f08fa54f23fde
SANCHIT BANSAL <sanchitbansal10@gmail.com>
2017-12-16 11:15:20 +0530

    Accidently made the last commit to master branch
    will create a new branch and suggest the changes there

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,2 @@
-
-const ConstantModule = function(){
-  const API_LINK = 'https://api.jamendo.com/v3.0';  //the private variable
-  const API_FORMAT = '?client_id=5adf7db0&format=jsonpretty' //the private variable
-  return {
-    'API_LINK':API_LINK,
-    'API_FORMAT':API_FORMAT
-  }
-  
-export default ConstantModule();
+const API_LINK = 'https://api.jamendo.com/v3.0'; 
+const API_FORMAT = '?client_id=5adf7db0&format=jsonpretty' 
