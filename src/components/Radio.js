e854161efa83d24d8bb93d0c9efe1586f2ce2d73
abhishek71994 <abhishek71994@gmail.com>
2017-12-18 14:25:58 +0530

    Api calls from all the components

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -1,10 +1,27 @@
 import React,{ Component } from 'react';
 import '../style.css';
+const apiLink = 'https://api.jamendo.com/v3.0';
+const apiFormat = '?client_id=5adf7db0&format=jsonpretty';
 class Radio extends Component{
+	state={
+		result:[]
+	}
+	componentDidMount(){
+		fetch(`${apiLink}/radios/${apiFormat}`).then(res => res.json()).then(d => this.setState({result:d.results}));
+	}
 	render(){
 		return(
-			<div className="Grid">This is Radio Component</div>
-			);
+			<div className="Grid">
+				{this.state.result.map(alb=>(
+					<div className="GridItem" key={alb.id} >
+						<div className="itembox">
+							<img src={alb.image} alt={alb.name}></img>
+						</div>
+					</div>
+					))}
+			</div>
+			)
+		
 	}
 }
 export default Radio;
