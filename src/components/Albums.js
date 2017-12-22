b6f4e88fab06fe1da875a4402a24876952290c4a
sanchitbansal10 <sanchitbansal10@gmail.com>
2017-12-22 11:20:50 +0530

    linting

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,11 +1,15 @@
 import React,{ Component } from 'react';
 import '../style.css';
+
 const apiLink = 'https://api.jamendo.com/v3.0';
 const apiFormat = '?client_id=5adf7db0&format=jsonpretty';
 
 class Albums extends Component{
-	state={
+	constructor(){
+		super();
+		this.state={
 		result:[]
+		}
 	}
 	componentDidMount(){
 		fetch(`${apiLink}/albums/${apiFormat}`).then(res => res.json()).then(d => this.setState({result:d.results}));
@@ -17,7 +21,7 @@ class Albums extends Component{
 					<div className="GridItem" key={alb.id} >
 						<div className="itembox">
 							<h6>{alb.name}</h6>
-							<a href={alb.shorturl} target="_blank"><img src={alb.image} alt={alb.shorturl}></img></a>
+							<a href={alb.shorturl} target="_blank"><img src={alb.image} alt={alb.shorturl} /></a>
 						</div>
 					</div>
 					))}
