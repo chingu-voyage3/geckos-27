3b4926a6359d14630bc00740a899747b4246b9cd
sanchitbansal10 <sanchitbansal10@gmail.com>
2017-12-22 11:04:17 +0530

    fixed linting issues

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,11 +1,15 @@
 import React,{ Component } from 'react';
 import '../style.css'; 
+
 const apiLink = 'https://api.jamendo.com/v3.0';
 const apiFormat = '?client_id=5adf7db0&format=jsonpretty';
 
 class Albums extends Component{
-	state={
-		result:[]
+	constructor(){
+		super()
+		this.state={
+			result:[]
+		}
 	}
 	componentDidMount(){
 		fetch(`${apiLink}/albums/${apiFormat}`).then(res => res.json()).then(d => this.setState({result:d.results}));
