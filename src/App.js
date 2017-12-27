2a9077599bcc4aa018698ae56718514a95c14e9f
abhishek71994 <abhishek71994@gmail.com>
2017-12-27 16:02:58 +0530

    semantic UI setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React,{Component} from 'react';
 import Albums from './components/Albums';
-import Sidebar from './components/Sidebar';
 import Radio from './components/Radio';
 import SongList from './components/Songlist';
 import Artists from './components/Artists';
 import AboutUs from './components/AboutUs';
 import RandomPlay from './components/RandomPlay'
 import Login from './components/Login';
+import * as FontAwesome from 'react-icons/lib/fa';
+import { Link } from 'react-router-dom';
 import { Switch,Route } from 'react-router-dom';
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
 import fire from './fire';
 
 class App extends Component{
 	constructor(props){
 		super(props);
 		this.state={
-			signedIn: false
+			signedIn: false,
+			visible:false
 		}
+	}
+	signOut(){
+		fire.auth().signOut().then(()=>{
+			console.log("Signed out.")
+		}).catch((err)=>{
+			console.log(err);
+		})
 	}
 
 	componentWillMount(){
@@ -30,7 +40,7 @@ class App extends Component{
 			}
 		});
 	}
-	
+	toggle = ()=>this.setState({visible:!this.state.visible})
 	render(){
 		console.log(this.state);
 		// Api.GetAlbums;
@@ -38,18 +48,71 @@ class App extends Component{
 			<div>
 				{this.state.signedIn?
 				<div>
-					<Sidebar/>
-					<Switch>
-						<div className="GridSection">
-							<Route exact path="/albums" component={Albums} />
-							<Route exact path="/radio" component={Radio} />
-							<Route exact path="/artists" component={Artists} />
-							<Route exact path="/aboutus" component={AboutUs} />
-              				<Route exact path="/g/:albid" component={SongList}/>
-							<Route exact path="/randomplay" component={RandomPlay} />
-            			</div>
-					</Switch>
-			</div>:
+			        <button className="spotIcon" onClick={this.toggle}>
+						<FontAwesome.FaSpotify
+							size={50}/>
+					</button>
+			        <Sidebar.Pushable as={Segment}>
+			          <Sidebar as={Menu} animation='overlay' width='thin' visible={this.state.visible} icon='labeled' vertical inverted>
+			          	<Menu.Item>
+			          		<h2>Findify</h2>
+			          	</Menu.Item>
+			          	<Menu.Item>
+				          	<form className="searchSection">
+							  <label>
+							    <input className="searchInput" type="text" name="name" placeholder="Search"/>
+							  </label>
+							  <button className="searchSubmit">
+									<FontAwesome.FaSearch
+										size={15}/>
+							  </button>
+							</form>
+						</Menu.Item>		
+			            <Menu.Item name='home'>
+			              <Link to='/' className="menuElements">Browse</Link>
+			            </Menu.Item>
+			            <Menu.Item>
+			            	<Link to='/radio' className="menuElements">Radio</Link>
+			            </Menu.Item>
+			            <Menu.Item>
+			            	<Link to='/albums' className="menuElements">Albums</Link>
+			            </Menu.Item>
+			            <Menu.Item>
+			            	<Link to='/artists' className="menuElements">Artists</Link>
+			            </Menu.Item>
+			            <Menu.Item>
+			            	<Link to='/randomplay' className="menuElements">Most Famous</Link>
+			            </Menu.Item>
+			            <Menu.Item>
+			            	<Link to='/aboutus' className="menuElements">About Us</Link>
+			            </Menu.Item>
+			            <Menu.Item>
+			            	<a href="#" className="menuElements">Github</a>
+			            </Menu.Item>
+			            <Menu.Item>
+			            	<Button onClick={this.signOut.bind(this)}>LogOut</Button>
+			            </Menu.Item>
+			          </Sidebar>
+			          <Sidebar.Pusher className="sem">
+			            <Segment basic>
+			                <div className="GridSection">
+							<Sidebar/>
+								<Switch>
+									<div >
+										<Route exact path="/albums" component={Albums} />
+										<Route exact path="/radio" component={Radio} />
+										<Route exact path="/artists" component={Artists} />
+										<Route exact path="/aboutus" component={AboutUs} />
+			              				<Route exact path="/g/:albid" component={SongList}/>
+										<Route exact path="/randomplay" component={RandomPlay} />
+			            			</div>
+								</Switch>
+							</div>
+			            </Segment>
+			          </Sidebar.Pusher>
+			        </Sidebar.Pushable>
+			      </div>
+				:
 				<Login/>
 			}
 			</div>
